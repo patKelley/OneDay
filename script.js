aa3b8fa27d1dfcbbfbdcc654702f834df0238748
patKelley <44733429+patKelley@users.noreply.github.com>
2018-11-03 15:21:51 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("button")
+.addEventListener("click", function(){
+    document.body.style.backgroundColor = "blue"
+    document.getElementById("sloth").style.color ="hotpink"
+    document.getElementById("header").innerHTML = <h1>"No Longer a Zoo"</h1>
+}) 
